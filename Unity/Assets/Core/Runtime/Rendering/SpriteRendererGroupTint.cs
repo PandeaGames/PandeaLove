fa92b8d7faa13943d2886242ce9b556b547a9e42
DESKTOP-SOVFDA4\ccove <ccove@pandeagames.com>
2017-12-30 20:42:40 -0800

    adding new scene that will be the proto world. Adding art for the stages.

---
+++ Unity/Assets/Core/Runtime/Rendering/SpriteRendererGroupTint.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpriteRendererGroupTint : MonoBehaviour
+{
+    [SerializeField]
+    private Color _color;
+
+    private SpriteRenderer[] _spriteRenderers;
+
+    // Use this for initialization
+    void Awake()
+    {
+        _spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Tint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Tint()
+    {
+        foreach (SpriteRenderer spriteRenderer in _spriteRenderers)
+        {
+            spriteRenderer.color = _color;
+        }
+    }
+}
